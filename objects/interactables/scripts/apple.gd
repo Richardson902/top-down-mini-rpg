19e202bc79246d106c469fd72f8b9f270ab9edc9
Richardson902 <160741608+Richardson902@users.noreply.github.com>
2025-01-31 12:11:05 -0400

    Added apple pickup sound

--- objects/interactables/scripts/apple.gd
+++ objects/interactables/scripts/apple.gd
@@ -1,14 +1,13 @@
-extends StaticBody2D
+extends Area2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var audio_player: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 func _ready() -> void:
 	fall_from_tree()
 
 func fall_from_tree() -> void:
 	animation_player.play("falling_apple")
-	
-func _on_area_2d_body_entered(body: Node2D) -> void:
+
+func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
 		PlayerHud.increment_collection(1)
-		queue_free()
+		animation_player.play("pickup")
