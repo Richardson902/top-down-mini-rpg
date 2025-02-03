7a2e5e32084c4b1a2dea4f9ca1824dbf88e7a608
Richardson902 <160741608+Richardson902@users.noreply.github.com>
2025-02-03 12:59:10 -0400

    added enemy health

---
+++ common/combat/health_component.gd
@@ -0,0 +1,17 @@
+class_name HealthComponent extends Node2D
+
+@export var MAX_HEALTH : float = 10.0
+@export var health_bar : ProgressBar
+var health : float
+
+
+func _ready() -> void:
+	health = MAX_HEALTH
+	health_bar.max_value = MAX_HEALTH
+	health_bar.value = health
+
+#func damage(attack : Attack) -> void:
+	#health -= attach.attack_damage
+	#
+	#if health <= 0:
+		#get_parent().queue_free()
---
+++ entities/enemies/enemy.gd
@@ -0,0 +1,6 @@
+class_name Enemy extends CharacterBody2D
+
+signal enemy_damaged()
+
+var direction : Vector2 = Vector2.DOWN
+var cardinal_direction : Vector2 = Vector2.ZERO
